e012b817b5c67f867b8c7cafc2896760888500cc
ByunghunKim <byunghun@publy.co>
2024-02-07 19:12:30 +0900

    feat: nextConfig 수정   - rewrite 제거

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const BASE_URL = process.env.API_URL || "";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites: async () => [
-    {
-      source: "/api/:path*",
-      destination: `${BASE_URL}/:path*`,
-    },
-  ],
   images: {
     remotePatterns: [
       {
