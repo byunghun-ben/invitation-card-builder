ee064bfaad3986770ab6f7c598879136d10b425f
Junghyun Son <json9512@gmail.com>
2024-02-08 03:52:27 +0900

    change config and add env example

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
-const BASE_URL = process.env.API_URL || "";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites: async () => [
-    {
-      source: "/api/:path*",
-      destination: `${BASE_URL}/:path*`,
-    },
-  ],
 };
 
 module.exports = nextConfig;
