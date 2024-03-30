c2b2d0900baf553bf25a019e4b91459e3afbfec0
ByunghunKim <byunghun@publy.co>
2024-03-30 17:52:55 +0900

    보안 취약점 해결 및 종속성 업데이트

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false, // 보안 취약점으로 취급되는 X-Powered-By 헤더를 제거한다.
+  // eslint: {
+  //   ignoreDuringBuilds: true, // 빌드 시에 eslint를 무시한다. (빌드 시간 단축)
+  // },
   images: {
     remotePatterns: [
       {
