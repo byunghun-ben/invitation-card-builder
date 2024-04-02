943f00f8f2dea6ec34e23087051c4f2dc5d148ac
ByunghunKim <byunghun@publy.co>
2024-04-02 22:56:06 +0900

    리다이렉트가 무한히 일어나는 문제를 해결했습니다.

--- src/app/ui/NavSection.tsx
+++ src/app/ui/NavSection.tsx
@@ -1,5 +1,10 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
 import { User } from "@supabase/supabase-js";
+import { revalidatePath } from "next/cache";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 type Props = {
   user: User | null;
@@ -10,12 +15,29 @@ const NavSection = ({ user }: Props) => {
     <section className="py-20">
       <div className="flex flex-col gap-4 items-center">
         {user ? (
-          <Link
-            href="/edit"
-            className="border border-slate-700 rounded-full py-2 px-4"
-          >
-            청첩장 수정하기
-          </Link>
+          <>
+            <Link
+              href="/edit"
+              className="border border-slate-700 rounded-full py-2 px-4"
+            >
+              청첩장 수정하기
+            </Link>
+
+            <button
+              type="button"
+              className="border border-slate-700 rounded-full py-2 px-4"
+              onClick={() => {
+                const supabase = createClient();
+
+                supabase.auth.signOut();
+
+                revalidatePath("/");
+                redirect("/");
+              }}
+            >
+              로그아웃
+            </button>
+          </>
         ) : (
           <>
             <Link
