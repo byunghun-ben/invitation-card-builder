511b3a8c3c3364e0d8c01f1aaba4f7d7b8ce6f58
ByunghunKim <byunghun@publy.co>
2024-04-02 22:37:11 +0900

    홈 배너 리스트 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import Header from "@/components/Header";
-import NavSection from "./ui/NavSection";
+import Image from "next/image";
 import { getSupabaseUser } from "./action";
+import HomeBannerList from "./ui/HomeBannerList";
+import NavSection from "./ui/NavSection";
+
+const BANNER_IMAGES = [
+  "https://knuahpfeiqewcczgflkw.supabase.co/storage/v1/object/public/images/banner_1.webp",
+  "https://knuahpfeiqewcczgflkw.supabase.co/storage/v1/object/public/images/banner_2.webp",
+  "https://knuahpfeiqewcczgflkw.supabase.co/storage/v1/object/public/images/banner_3.webp",
+];
 
 const Page = async () => {
   const user = await getSupabaseUser();
@@ -8,9 +16,21 @@ const Page = async () => {
   return (
     <div className="flex flex-col">
       <Header />
-      <section className="h-[260px] border-b flex items-center justify-center bg-pink-200">
-        <h1 className="font-bold">우쥬 메크 청첩장 위드 어스?</h1>
-      </section>
+
+      <HomeBannerList>
+        {BANNER_IMAGES.map((url, index) => (
+          <li key={url} className="relative flex-none h-full w-full snap-start">
+            <Image
+              src={url}
+              className="w-full h-full object-cover"
+              alt="banner"
+              fill
+              draggable={false}
+              priority={index === 0}
+            />
+          </li>
+        ))}
+      </HomeBannerList>
       <NavSection user={user} />
     </div>
   );
