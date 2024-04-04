e0ef31155fcb7fe458bded4c33baa2d49c496222
ByunghunKim <byunghun@publy.co>
2024-04-04 11:29:54 +0900

    CSS 및 로딩 컴포넌트 수정

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,10 @@
 export const Loading = () => {
   return (
-    <div className="lds-ellipsis inline-block relative w-12 h-3">
+    <div className="lds-ellipsis inline-block relative w-10 h-3">
       <div className="absolute top-0.5 left-0 w-2 h-2 rounded-full bg-current" />
       <div className="absolute top-0.5 left-0 w-2 h-2 rounded-full bg-current" />
-      <div className="absolute top-0.5 left-5 w-2 h-2 rounded-full bg-current" />
-      <div className="absolute top-0.5 left-10 w-2 h-2 rounded-full bg-current" />
+      <div className="absolute top-0.5 left-4 w-2 h-2 rounded-full bg-current" />
+      <div className="absolute top-0.5 left-8 w-2 h-2 rounded-full bg-current" />
     </div>
   );
 };
