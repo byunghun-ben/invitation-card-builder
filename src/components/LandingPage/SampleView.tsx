002b2da8408215d8d05350575357950133ad2631
ByunghunKim <byunghun@publy.co>
2024-08-05 11:55:25 +0900

    fix: 환경변수

--- src/components/LandingPage/SampleView.tsx
+++ src/components/LandingPage/SampleView.tsx
@@ -3,9 +3,7 @@
 import { SyntheticEvent, useRef, useState } from "react";
 import { Loading } from "../Loading";
 
-const URL =
-  process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL ??
-  "http://localhost:3000";
+const URL = process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000";
 const IFRAME_URL = `${URL}/sample`;
 
 const SampleView = () => {
